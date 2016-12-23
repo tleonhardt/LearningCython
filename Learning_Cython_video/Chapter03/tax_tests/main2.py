e122a665d068d9f0091e45bdf6a05969abc90b93
Todd Leonhardt <tleonhardt@sigovs.com>
2016-12-22 20:08:25 -0500

    Added comments to tax code tests

--- Learning_Cython_video/Chapter03/tax_tests/main2.py
+++ Learning_Cython_video/Chapter03/tax_tests/main2.py
@@ -1,8 +1,12 @@
 
-# Taxable income of 11 million people
+""" Taxable income of 11 million people.
+
+This is a refactored version of main.py which is more suitable for testing.
+"""
 
 import numpy
 from numpy.random import RandomState
+# Ensure the same set of random numbers are generated each time
 prng = RandomState(1)
 taxable_incomes = prng.uniform(
         5e3, 3e5, int(11e6))
@@ -14,12 +18,14 @@ taxable_incomes = prng.uniform(
 import taxcode
 
 def tot():
+    """ Compute the total tax """
     total_tax = 0
     for income in taxable_incomes:
         total_tax += taxcode.tax_table(income)
     return total_tax
 
 def run():
+    """ main() runner """
     total_tax = tot()
     print("""
         Australian Tax due for 2015/2016
--- Learning_Cython_video/Chapter03/tax_tests/test_taxcode.py
+++ Learning_Cython_video/Chapter03/tax_tests/test_taxcode.py
@@ -1,34 +1,48 @@
+""" Pytest test file.
+
+To run these tests, at the command line, run:
+    pytest
+or
+    py.test
+"""
 from taxcode import tax_table
 import numpy
 import pytest
 
 def test_notax():
+    """ Test when a low income is provided - calculated tax should be zero """
     ans = tax_table(500)
+    # Check that the tax is very close to zero to deal with floating-point precision issues
     numpy.testing.assert_allclose(ans, 0)
 
 def test_notax_edge():
+    """ Check edge case on low tax, from below """
     ans = tax_table(18200)
     numpy.testing.assert_allclose(ans, 0)
 
 def test_notax_over_edge():
+    """ Check edge case on low tax, from above """
     ans = tax_table(18201)
     numpy.testing.assert_allclose(ans, 0.19)
 
 def test_str():
+    """ Ensure TypeError gets raised if someone passes a string in """
     with pytest.raises(TypeError) as excinfo:
         ans = tax_table('nonsense')
 
 def test_nothing():
+    """ Verify that TypeError gets raised if function called with no arguments """
     with pytest.raises(TypeError) as excinfo:
         ans = tax_table()
 
 def test_negative():
+    """ Make sure negative input values are dealt with appropriately """
     ans = tax_table(-1e6)
     numpy.testing.assert_allclose(ans, 0)
 
 def test_main():
+    """ Verify the final tax result is correct """
     import main2
     ans = main2.tot()
     numpy.testing.assert_allclose(
             ans, 517474750456.53094)
-
